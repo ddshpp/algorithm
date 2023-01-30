a21a306ac54e830b8582cfa46cfc54f460fd7da5
ddshpp <dshp753@gmail.com>
2023-01-30 17:29:43 +0900

    문제풀이 업로드 테스트

--- src/main/java/baekjoon/bronze/no_1000.java
+++ src/main/java/baekjoon/bronze/no_1000.java
@@ -1,7 +1,20 @@
 package baekjoon.bronze;
 
-public class no_1000 {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
 
+        //숫자입력받기(공백이 있는듯)
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        //공백제거
+        String[] input2 = input.split(" ");
+
+        int a = Integer.parseInt(input2[0]);
+        int b = Integer.parseInt(input2[1]);
+
+        System.out.println(a+b);
     }
 }
