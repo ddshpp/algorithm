560e4cc9678ddd154bb8713db2c6e0fa2ce010e8
ddshpp <dshp753@gmail.com>
2023-02-01 19:47:44 +0900

    20230201 문제풀이 추가

--- src/main/java/baekjoon/bronze/no_1008.java
+++ src/main/java/baekjoon/bronze/no_1008.java
@@ -1,16 +1,16 @@
 package baekjoon.bronze;
 
 import java.util.Scanner;
-public class no_1001 {
+public class no_1008 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         String[] input2 = input.split(" ");
 
-        int a = Integer.parseInt(input2[0]);
-        int b = Integer.parseInt(input2[1]);
+        double a = Double.parseDouble(input2[0]);
+        double b = Double.parseDouble(input2[1]);
 
-        System.out.println(a - b);
+        System.out.println(a / b);
     }
 }
--- src/main/java/baekjoon/bronze/no_1152.java
+++ src/main/java/baekjoon/bronze/no_1152.java
@@ -1,19 +1,20 @@
 package baekjoon.bronze;
 
 import java.util.Scanner;
-public class no_1000 {
+
+public class no_1152 {
     public static void main(String[] args) {
 
-        //숫자입력받기(공백이 있는듯)
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        input = input.trim();
 
-        //공백제거
         String[] input2 = input.split(" ");
 
-        int a = Integer.parseInt(input2[0]);
-        int b = Integer.parseInt(input2[1]);
-
-        System.out.println(a + b);
+        if (input.isEmpty() == true) {
+            System.out.println(0);
+        } else {
+            System.out.println(input2.length);
+        }
     }
 }
